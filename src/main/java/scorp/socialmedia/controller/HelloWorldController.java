9d51201f1757c8d166667b57fda9ac88d2325127
sefakrb <sefa799@gmail.com>
2024-09-05 17:33:44 +0300

    dockerize and try ci pipeline with github actions

---
+++ src/main/java/scorp/socialmedia/controller/HelloWorldController.java
@@ -0,0 +1,15 @@
+package scorp.socialmedia.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
+public class HelloWorldController {
+    @GetMapping
+    public Integer sum()
+    {
+        return 2 + 2;
+    }
+}
--- src/test/java/scorp/socialmedia/SocialmediaApplicationTests.java
+++ src/test/java/scorp/socialmedia/SocialmediaApplicationTests.java
@@ -1,13 +1,28 @@
 package scorp.socialmedia;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import scorp.socialmedia.controller.HelloWorldController;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class SocialmediaApplicationTests {
 
+	@Autowired
+	private HelloWorldController helloWorldController;
+
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	void testHelloWorldController(){
+		Integer sumResult = helloWorldController.sum();
+		Integer result = 4;
+
+		assertThat(sumResult).isEqualTo(result);
 	}
 
 }
